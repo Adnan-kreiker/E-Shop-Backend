de2c4caad658a47c9082d884589df34028cb8a3e
Adnan-kreiker <adnankreiker@hotmail.com>
2022-02-21 14:50:21 +0200

    edited config

--- config/database.js
+++ config/database.js
@@ -1,40 +1,19 @@
-const parse = require("pg-connection-string").parse
-const path = require("path")
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => {
-  if (env("NODE_ENB") === "production") {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: "default",
-      connections: {
-        defailt: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgress",
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            ssl: false,
-          },
-        },
-      },
-    }
-  }
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "sqlite",
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
-      },
-      useNullAsDefault: true,
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
+    debug: false
   }
-}
+});
