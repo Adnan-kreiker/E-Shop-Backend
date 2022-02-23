0dd99cdb0f488d0ca44b4113b86986e9167fe10f
Adnan-kreiker <adnankreiker@hotmail.com>
2022-02-23 17:54:41 +0200

    updated the strapi provider's dependency

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -6,6 +6,10 @@ module.exports = ({ env }) => ({
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_API_KEY"),
         api_secret: env("CLOUDINARY_API_SECRET")
+      },
+      actionOptions: {
+        upload: {},
+        delete: {}
       }
     }
   }
