a5b1279a5648764e8653d06e17f7a5df91312951
Adnan-kreiker <adnankreiker@hotmail.com>
2022-02-23 17:04:48 +0200

    fixed the env variables

--- config/plugins.js
+++ config/plugins.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
       provider: "cloudinary",
       providerOptions: {
         cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET")
+        api_key: env("CLOUDINARY_API_KEY"),
+        api_secret: env("CLOUDINARY_API_SECRET")
       },
       actionOptions: {
         upload: {},
