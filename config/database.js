a173091c2f1d3f4e8ba72be3d6b085899d1e3709
Adnan-kreiker <adnankreiker@hotmail.com>
2022-02-23 15:24:26 +0200

    added cloudinary plugin

--- config/database.js
+++ config/database.js
@@ -1,19 +1,40 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const parse = require("pg-connection-string").parse
+const path = require("path")
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
-      }
-    },
-    debug: false
+module.exports = ({ env }) => {
+  if (env("NODE_ENB") === "production") {
+    const config = parse(process.env.DATABASE_URL)
+    return {
+      defaultConnection: "default",
+      connections: {
+        defailt: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgress",
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    }
   }
-});
+  return {
+    connection: {
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
+    },
+  }
+}
--- config/server.js
+++ config/server.js
@@ -1,3 +1,4 @@
 module.exports = ({ env }) => ({
-  url: env("MY_HEROKU_URL")
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
 });
