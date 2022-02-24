ea3da28a4f703bd3eb4c55b8839602280f21c6c0
Adnan-kreiker <adnankreiker@hotmail.com>
2022-02-24 10:26:40 +0200

    updated the gql configs

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -18,7 +18,7 @@ module.exports = ({ env }) => ({
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: false,
-      depthLimit: 7,
+      depthLimit: 8,
       amountLimit: 100,
       apolloServer: {
         tracing: false
